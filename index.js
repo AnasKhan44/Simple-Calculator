89ca4e75adc5bd4326e15294606fa0c6fc91da68
AnasKhan44 <anaskhan.ak2005@gmail.com>
2024-04-03 23:31:10 +0500

    Pushed Calculator To Github

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([{ message: "Enter First Number", type: "number", name: "num1" },
+    { message: "Enter Second Number", type: "number", name: "num2" },
+    { message: "Select Operation you want to perform: ", type: "list", name: "operator", choices: ["Add", "Sub", "Div", "Mul"] }
+]);
+if (answer.operator === "Add") {
+    console.log(`${answer.num1} + ${answer.num2} = ${answer.num1 + answer.num2}`);
+}
+else if (answer.operator === "Sub") {
+    console.log(`${answer.num1} - ${answer.num2} = ${answer.num1 - answer.num2}`);
+}
+else if (answer.operator === "Div") {
+    console.log(`${answer.num1} / ${answer.num2} = ${answer.num1 / answer.num2}`);
+}
+else
+    console.log(`${answer.num1} * ${answer.num2} = ${answer.num1 * answer.num2}`);
